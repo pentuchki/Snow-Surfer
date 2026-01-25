907eca6680a1464a97c26fe8b39ea80cc348c70c
pentuchki <arslan.umutcan@gmail.com>
2026-01-25 22:47:51 +0300

    Added restart the level function after reaching the finish line or after a clash

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
 
         if (collision.gameObject.layer == layerIndex)
     {
-        SceneManager.LoadScene(0);
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+        Invoke("ReloadScene", restartDelay);
+        //SceneManager.LoadScene(0);
     }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,13 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            Invoke("ReloadScene", restartDelay);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
