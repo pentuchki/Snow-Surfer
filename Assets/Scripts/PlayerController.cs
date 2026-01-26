24faae6f026ddb77fd42b73c1db2ba60e09e1333
pentuchki <arslan.umutcan@gmail.com>
2026-01-26 23:44:15 +0300

    Added speed boost function to w key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,28 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 24.33f;
+    [SerializeField] float boostSpeed = 36.45f;
+
     InputAction moveAction;
     Rigidbody2D myRigidBody2D;
+    SurfaceEffector2D surfaceEffector2D;
+    Vector2 moveVector;
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
 
         if(moveVector.x < 0)
@@ -25,6 +36,17 @@ public class PlayerController : MonoBehaviour
         else if(moveVector.x > 0)
         {
         myRigidBody2D.AddTorque(-torqueAmount);
+        }
+    }
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
